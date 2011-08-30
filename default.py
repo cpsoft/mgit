326333147ee8045fac0a37fe10de71bf3ccb4dfe
Zhu Hao <cpsoft@msn.com>
2011-08-30 17:35:50 +0800

    fixed command args contain space char in default command.

--- default.py
+++ default.py
@@ -5,27 +5,21 @@ from subproject import SubProject
 
 class Default():
 	def __init__(self):
-		self.parser = OptionParser()
-		self.parser.add_option("-C", "--repo-dir", dest="repodir", help="repo directory")
-		self.parser.set_usage("mgit <command>")
+		return
 
 	def print_usage(self):
-		self.parser.print_usage()
-	
+		return
+
 	def func(self, subproject):
 		cmd = ['mgit'] + self.args
 		os.system(" ".join(cmd))
 	
 	def run(self, args):
-		self.options,self.args = self.parser.parse_args(args)
-		cwd = os.getcwd()
-		if self.options.repodir != None:
-			os.chdir("/".join([cwd, self.options.repodir]))
-		cmd = ['git'] + self.args
-		cmd = " ".join(cmd)
-		os.system(cmd)
-
+		for i in xrange(0, len(args)):
+			if args[i].find(" ") > 0:
+				args[i]="\"%s\"" % args[i]
+		self.args = args
 		self.project = Project()
+		cmd = ['git'] + self.args
+		os.system(" ".join(cmd))
 		self.project.inSubProject(self)
-		os.chdir(cwd)
-		
